d7c3d51a2376448b5618ba3a02cc019dbc33481e
Shubhi Agarwal <shubhiagarwal1612@gmail.com>
2017-10-24 00:47:23 +0530

    Add files via upload
    Code that reads an image and output is in the form of a matrix

---
+++ readimg.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2
+
+# Load an color image in grayscale
+img = cv2.imread('Shubhipic.jpg',0)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows() # If 0 is passed, it waits indefinitely for a key stroke.
