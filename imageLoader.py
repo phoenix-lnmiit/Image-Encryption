2c788fadc401def7b274944ec58b1f79faeb8414
Kanishk Sharma <kanishk.3107@gmail.com>
2017-10-24 02:05:33 +0530

    Update imageLoader.py

--- imageLoader.py
+++ imageLoader.py
@@ -1,5 +1,9 @@
 import cv2
 
 img = cv2.imread('E:/OpenCV/Images/maxresdefault.jpg')  #path and name of the image
+
+#To choose a pixel with co-ordinates (x,y) type 
+#px = img[x][y]
+
 cv2.imshow('ImageWindow', img)
 cv2.waitKey()
