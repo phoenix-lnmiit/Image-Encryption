d19ba7d6baa4a7aedf66103665ab0abcd976313d
Kanishk Sharma <kanishk.3107@gmail.com>
2017-10-30 03:48:44 +0530

    Add files via upload

---
+++ Task2/task2.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread('E:/OpenCV/Images/inputImage.jpg',cv2.IMREAD_GRAYSCALE)
+
+#Matrix to hold intensity values
+matrix = img[0:, 0:]
+print(2*matrix)
+img[0:, 0:] = 2*matrix
+
+cv2.imshow('ImageWindow', img)
+
+cv2.imwrite('E:/OpenCV/Images/outputImage.jpg', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
