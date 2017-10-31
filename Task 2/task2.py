c03c6be413c764c99d66da383c782db86b77d729
UC07 <31630178+UC07@users.noreply.github.com>
2017-10-31 23:35:27 +0530

    Yeah I have done..!!!

---
+++ Task 2/task2.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread('inputImage.jpg',cv2.IMREAD_GRAYSCALE)
+
+matrix = img[0:,0:]
+print(matrix)
+
+img[0:,0:] = 2*matrix
+
+cv2.imshow('Image Window',img)
+cv2.imwrite('outputImage.jpg',img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
