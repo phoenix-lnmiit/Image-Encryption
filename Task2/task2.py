f97c6f6ef93656d6589e191cfd44134074b0c8dc
Kanishk Sharma <kanishk.3107@gmail.com>
2017-10-31 23:45:04 +0530

    Update task2.py

--- Task2/task2.py
+++ Task2/task2.py
@@ -2,13 +2,12 @@ import cv2
 
 img = cv2.imread('inputImage.jpg',cv2.IMREAD_GRAYSCALE)
 
-#Matrix to hold intensity values
 matrix = img[0:, 0:]
 print(matrix)
+
 img[0:, 0:] = 2*matrix
 
 cv2.imshow('ImageWindow', img)
-
 cv2.imwrite('outputImage.jpg', img)
 
 cv2.waitKey(0)
